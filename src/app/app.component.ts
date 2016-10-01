38a6940efc76e37e84552fc00acc4631ddfbc351
feitzi <mathias@feitzinger.org>
2016-10-01 08:55:44 +0200

    working version with angular2 release version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,45 +1,7 @@
-import {
-    Component,
-    trigger,
-    state,
-    style,
-    transition,
-    animate
-} from '@angular/core';
-const ipcRenderer = require('electron').ipcRenderer;
+import { Component } from '@angular/core';
 
 @Component({
-    moduleId: module.id,
-    selector: 'my-app',
-    template: `
-  <div class="container" @state="componentState">
-    <h1 #myList>{{text_hello_world}} <small>{{text_small}}</small></h1>
-    <input type="text" class="form-control" [(ngModel)]="text_small">
-    <br/>
-    <button (click)="ping()">Ping</button>
-  </div>
-  `,
-    animations: [
-        trigger("state", [
-            transition("void => *", [
-                style({transform: 'translateX(-100%)'}),
-                animate(100)
-            ])
-        ])
-    ]
+  selector: 'my-app',
+  template: '<h1>My First Angular App</h1>'
 })
-export class AppComponent {
-    private text_hello_world: string = "Hello Angular 2!";
-    private text_small: string = "Greatness awaits..."
-    private componentState: string = "active";
-
-    constructor() {
-        ipcRenderer.on('reply', (sender, arg) => {
-            alert(`Reply was ${arg}`);
-        });
-    }
-
-    ping() {
-        ipcRenderer.send('message', 'ping');
-    }
-}
+export class AppComponent { }
--- src/systemjs.config.js
+++ src/systemjs.config.js
@@ -1,52 +1,43 @@
-(function(global) {
-
-  // map tells the System loader where to look for things
-  var map = {
-      'app': 'app',
-      'rxjs': 'assets/vendor/rxjs',
-      'angular2-in-memory-web-api': 'assets/vendor/angular2-in-memory-web-api',
-      '@angular': 'assets/vendor/@angular'
-  };
-  // packages tells the System loader how to load when no filename and/or no extension
-  var packages = {
-      'app': {
-          main: 'boot.js',
-          defaultExtension: 'js'
+/**
+ * System configuration for Angular samples
+ * Adjust as necessary for your application needs.
+ */
+(function (global) {
+  System.config({
+    paths: {
+      // paths serve as alias
+      'npm:': '../node_modules/'
+    },
+    // map tells the System loader where to look for things
+    map: {
+      // our app is within the app folder
+      app: 'app',
+      // angular bundles
+      '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
+      '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
+      '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
+      '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
+      '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+      '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
+      '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
+      '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
+      // other libraries
+      'rxjs':                      'npm:rxjs',
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api',
+    },
+    // packages tells the System loader how to load when no filename and/or no extension
+    packages: {
+      app: {
+        main: './main.js',
+        defaultExtension: 'js'
       },
-      'rxjs': {
-          defaultExtension: 'js'
+      rxjs: {
+        defaultExtension: 'js'
       },
-      'angular2-in-memory-web-api': {
-          defaultExtension: 'js'
+      'angular-in-memory-web-api': {
+        main: './index.js',
+        defaultExtension: 'js'
       }
-  };
-
-  var packageNames = [
-    '@angular/common',
-    '@angular/compiler',
-    '@angular/core',
-    '@angular/forms',
-    '@angular/http',
-    '@angular/platform-browser',
-    '@angular/platform-browser-dynamic',
-    '@angular/router',
-    '@angular/upgrade',
-    'core-js'
-  ];
-
-  // add package entries for angular packages in the form '@angular/common': { main: 'index.js', defaultExtension: 'js' }
-  packageNames.forEach(function(pkgName) {
-    packages[pkgName] = { main: 'index.js', defaultExtension: 'js' };
+    }
   });
-
-  var config = {
-    map: map,
-    packages: packages
-  }
-
-  // filterSystemConfig - index.html's chance to modify config before we register it.
-  if (global.filterSystemConfig) { global.filterSystemConfig(config); }
-
-  System.config(config);
-
 })(this);
