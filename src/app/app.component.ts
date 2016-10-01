4eb211f81174187db606a5cb22d84c7c61238ad1
feitzi <mathias@feitzinger.org>
2016-10-01 13:25:47 +0200

    add orginal button and input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,26 @@
 import { Component } from '@angular/core';
+const ipcRenderer = require('electron').ipcRenderer;
 
 @Component({
+  moduleId: module.id,
   selector: 'my-app',
-  template: '<h1>My First Angular App</h1>'
+  templateUrl: 'app.component.html'
 })
-export class AppComponent { }
+export class AppComponent {
+
+
+  constructor() {
+    ipcRenderer.on("reply", (event, arg) => {
+      console.log("Reply was " + arg);
+    });
+  }
+
+  public text_hello_world: string = "Hello Angular 2!";
+  public text_small: string = "Greatness awaits..."
+
+  public test(): void {
+    console.log("Getestet");
+    ipcRenderer.send("message", "tested");
+  }
+
+}
